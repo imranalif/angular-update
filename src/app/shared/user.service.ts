29cf45a4d741f4f734463ce6744311b656b4cc6d
imranalif <imrancsiteng@gmail.com>
2019-07-01 04:48:28 -0700

    3 update

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,62 +1,66 @@
-import { Injectable } from '@angular/core';
-import {IUser} from 'src/app/shared/user.model';
-import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { Injectable } from "@angular/core";
+import { IUser } from "src/app/shared/user.model";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
-  myForm:FormGroup;
-  user:IUser;
-  private listUser:IUser[]=[
-    {id:1,firstName:'imranfgh',lastName:'khan',phone:'0178255',email:'imra@gmail.com',address:'moragacha',password:'123456',confirmPassword:'123456'},
-    {id:2,firstName:'alif',lastName:'khan',phone:'0178255',email:'imra@gmail.com',address:'moragacha',password:'123456',confirmPassword:'123456'} 
-   ];
-   getUser():IUser[]{
-     return this.listUser;
-   }
-   save(user:IUser){
+  myForm: FormGroup;
+  user: IUser;
+  private listUser: IUser[] = [
+    {
+      id: 1,
+      firstName: "imranfgh",
+      lastName: "khan",
+      phone: "0178255",
+      email: "imra@gmail.com",
+      address: "moragacha",
+      password: "123456",
+      confirmPassword: "123456"
+    },
+    {
+      id: 2,
+      firstName: "alif",
+      lastName: "khan",
+      phone: "0178255",
+      email: "imra@gmail.com",
+      address: "moragacha",
+      password: "123456",
+      confirmPassword: "123456"
+    }
+  ];
 
+  getUser(): IUser[] {
+    return this.listUser;
+  }
 
-  if(user.id===null){
-  const maxId= this.listUser.reduce(function(e1,e2){
-      return (e1.id>e1.id) ?e1:e2;
-    }).id;
-    user.id=maxId+1;
-   
-     return this.listUser.push(user)}
+  save(user: IUser) {
+    if (user.id === null) {
+      const maxId = this.listUser.reduce(function(e1, e2) {
+        return e1.id > e1.id ? e1 : e2;
+      }).id;
+      user.id = maxId + 1;
+      return this.listUser.push(user);
+    } else {
+      const foundIndex = this.listUser.findIndex(e => e.id === user.id);
+      this.listUser[foundIndex] = user;
+    }
+  }
 
+  getUserId(id: number): IUser {
+    return this.listUser.find(e => e.id === id);
+  }
 
-     else{
-     const foundIndex= this.listUser.findIndex(e=>e.id===user.id)
-     this.listUser[foundIndex]=user;
-     }
-
-   }
-   getUserId(id:number) : IUser{
-    return this.listUser.find(e=>e.id===id);
-   
-   }
-   delete(id:number,data){
-const i=data.findIndex(e=>e.id===id)
-if(i!= -1){
-  data.splice(i,1);
- 
-}
-   }
-   
-  constructor(private fb:FormBuilder) {
-    this.myForm=this.fb.group({
-      id:[null],
-      firstName:['',[Validators.required,Validators.minLength(5)]],
-      lastName:[''],
-      email:['',[Validators.required,Validators.email]],
-      phone:['',[Validators.required,Validators.minLength(5)]],
-      address:[''],
-      password:['',[Validators.required,Validators.minLength(5)],
-      
-    ]
-    })
-   }
- 
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      id: [null],
+      firstName: ["", [Validators.required, Validators.minLength(5)]],
+      lastName: [""],
+      email: ["", [Validators.required, Validators.email]],
+      phone: ["", [Validators.required, Validators.minLength(5)]],
+      address: [""],
+      password: ["", [Validators.required, Validators.minLength(5)]]
+    });
+  }
 }
--- src/app/user/create-user/create-user.component.ts
+++ src/app/user/create-user/create-user.component.ts
@@ -1,96 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,Validators } from '@angular/forms';
-//import{Router} from '@angular/router'
-import {UserService} from 'src/app/shared/user.service'
-import {IUser} from 'src/app/shared/user.model'
-import { ActivatedRoute } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { UserService } from "src/app/shared/user.service";
+import { IUser } from "src/app/shared/user.model";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-create-user',
-  templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  selector: "app-create-user",
+  templateUrl: "./create-user.component.html",
+  styleUrls: ["./create-user.component.css"]
 })
 export class CreateUserComponent implements OnInit {
-myForm:FormGroup;
-users:IUser[]=[];
+  myForm: FormGroup;
+  users: IUser[] = [];
 
-user:IUser={
-  id:null,
-  firstName:null,
-  lastName:null,
-  phone:null,
-  email:'',
-  address:null,
-  password:null,
-  confirmPassword:null
-}
+  user: IUser = {
+    id: null,
+    firstName: null,
+    lastName: null,
+    phone: null,
+    email: "",
+    address: null,
+    password: null,
+    confirmPassword: null
+  };
 
-  constructor(private fb:FormBuilder,private userService:UserService,private _route:ActivatedRoute) { }
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private _route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    
-    this.myForm=this.fb.group({
-      id:null,
-      firstName:['',[Validators.required,Validators.minLength(5)]],
-      lastName:[''],
-      email:['',[Validators.required,Validators.email]],
-      phone:['',[Validators.required,Validators.minLength(5)]],
-      address:[''],
-      password:['',[Validators.required,Validators.minLength(5)],
-      
-    ]
-    })
-    this._route.paramMap.subscribe(parameterMap=>{
-      const Id= +parameterMap.get('id');
+    this.myForm = this.fb.group({
+      id: [null],
+      firstName: ["", [Validators.required, Validators.minLength(5)]],
+      lastName: [""],
+      email: ["", [Validators.required, Validators.email]],
+      phone: ["", [Validators.required, Validators.minLength(5)]],
+      address: [""],
+      password: ["", [Validators.required, Validators.minLength(5)]]
+    });
+    this._route.paramMap.subscribe(parameterMap => {
+      const Id = +parameterMap.get("id");
       this.getUser(Id);
-      //console.log(this.user.id)
-      });
-      
-      
+    });
   }
-  
-  getUser(id:number){
-    if (id===0)
-    this.user={
-      id:null,
-      firstName:null,
-      lastName:null,
-      phone:null,
-      email:'',
-      address:null,
-      password:null,
-      confirmPassword:null
-    }
-    else
-     this.user=Object.assign({},this.userService.getUserId(id));
-     this.myForm.patchValue({
-       id:id,
-       firstName:this.user.firstName,
-       lastName:this.user.lastName,
-       phone:this.user.phone,
-       email:this.user.email,
-       address:this.user.address,
-       password:this.user.password,
-       confirmPassword:this.user.confirmPassword
-     })
-    // console.log(this.user);
-     
-   
+
+  getUser(id: number) {
+    /* if (id === null)
+      this.user = {
+        id: null,
+        firstName: null,
+        lastName: null,
+        phone: null,
+        email: "",
+        address: null,
+        password: null,
+        confirmPassword: null
+      };
+    else {*/
+    this.user = Object.assign({}, this.userService.getUserId(id));
+    this.myForm.setValue({
+      id: this.user.id,
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      phone: this.user.phone,
+      email: this.user.email,
+      address: this.user.address,
+      password: this.user.password,
+      confirmPassword: this.user.confirmPassword
+    });
+    //}
   }
-  
-  onSubmit(user){
-   
-   
-//this.users.push(user);
-//if(this.myForm.value.id==null)
-            this.userService.save(this.myForm.value);
 
-console.log(this.users);
-
-
+  onSubmit(user) {
+    this.userService.save(this.myForm.value);
+    this.router.navigate(["/user-details"]);
   }
- 
-
-  
 }
